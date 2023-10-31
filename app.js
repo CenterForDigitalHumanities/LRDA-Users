8b9f64b6f3d2cada1e036f3e87092fc9ea6a7f87
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-10-31 14:03:00 -0500

    Remove unused code and pages

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ const indexRouter = require('./routes/index.js')
 
 var app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
 //Middleware to use
 
 /**
